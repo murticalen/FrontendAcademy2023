b7f5987ea87eef2878949a0fbaac621e46a02328
Alen Murtić <murtic.alen@hotmail.com>
2023-03-15 19:32:31 +0100

    idk

--- Lessons/03-files/functionArgs.js
+++ Lessons/03-files/functionArgs.js
@@ -2,7 +2,7 @@ let number = 10
 let obj1 = { a: 'unchanged' }
 let obj2 = { a: 'unchanged' }
 
-function assignNewValues(num                          ber, obj3, obj4) {
+function assignNewValues(number, obj3, obj4) {
   number = number ** 2 // ** is exponential -> 10^2, numbers are pass by value -> no change
   obj3.a = 'changed' // direct assignment of a property -> changes reference object -> call by reference
   obj4 = { a: 'changed' } // new object assignment -> creates new object, doesn't change reference object -> call by value
